e1b0294834084909d958732a3d457c1c7ea03bf4
Tony Air <tony@twma.pro>
2023-03-24 15:46:11 +0200

    Initial

---
+++ src/Tasks/DumpMySQL.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace A2nt\LiveChecks\Tasks;
+
+use SilverStripe\Assets\File;
+use SilverStripe\Dev\BuildTask;
+use SilverStripe\ORM\DB;
+
+class DumpMySQL extends BuildTask
+{
+    protected $title = 'Dump MySQL Task';
+    protected $description = 'Create MySQL dump';
+    protected $enabled = true;
+
+    public function run($request)
+    {
+        $fileName = 'backup-'.date('d-m-Y').'.sql';
+        $cfg = DB::getConfig();
+
+        try {
+            ob_clean();
+        } catch (Exception $e) {
+        }
+
+        // check if gzip is on
+        try {
+            if (count(array_intersect(['mod_deflate', 'mod_gzip'], apache_get_modules())) > 0) {
+                $fileName .= '.gz';
+            }
+        } catch (Exception $e) {
+        }
+
+        header('Content-Disposition: attachment; filename="'.$fileName.'"');
+        passthru('mysqldump -u '.$cfg['username'].' --password="'.$cfg['password'].'" '.$cfg['database']);
+        exit(0);
+    }
+}
